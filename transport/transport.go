d05c90832dd396b3573fc6645a30bf2b3f726d9c
hongxing xu <hongxing@hongxings-MacBook-Pro.local>
2015-12-19 17:47:42 +0800

    step

--- transport/transport.go
+++ transport/transport.go
@@ -1,12 +1,42 @@
 // transport.go
-package transport 
+package transport
 
+import (
+	_ "errors"
+	"net"
+)
 
-
+type TransportType string
 
 const (
-	TransportTypeTCP = "tcp"
-	TransportTypeUDP = "udp"
-	TransportTypeSCTP = "sctp"
-	TransportTypeTLS = "tls"
+	TransportTypeTCP  TransportType = "tcp"
+	TransportTypeUDP                = "udp"
+	TransportTypeSCTP               = "sctp"
+	TransportTypeTLS                = "tls"
 )
+
+func (tt TransportType) String() string {
+	return string(tt)
+}
+
+type ListenServer struct {
+	TransportType TransportType
+	Listener      net.Listener
+}
+
+func (ls *ListenServer) String() string {
+	return ls.TransportType.String()
+}
+
+func createTcpServer(laddr string) (*ListenServer, error) {
+	server := &ListenServer{TransportType: TransportTypeTCP}
+
+	listener, err := net.Listen(string(TransportTypeTCP), laddr)
+	if err != nil {
+		return nil, err
+	}
+	server.Listener = listener
+
+	return server, nil
+
+}
